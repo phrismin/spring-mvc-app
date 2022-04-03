e28e5a11a6063c6a7776ec354d7d74287f6e6834
Alexey Rudoy <rudenw01@gmail.com>
2022-04-03 18:41:44 +0300

    feat: added valodation

--- src/main/java/com/rudoy/app/controller/ControllerPeople.java
+++ src/main/java/com/rudoy/app/controller/ControllerPeople.java
@@ -2,9 +2,11 @@ package com.rudoy.app.controller;
 
 import com.rudoy.app.dao.PersonDAO;
 import com.rudoy.app.model.Person;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -46,7 +48,11 @@ public class ControllerPeople {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -58,8 +64,12 @@ public class ControllerPeople {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person,
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult,
                          @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
         personDAO.update(id, person);
         return "redirect:/people";
     }
